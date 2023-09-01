9e1eee326e5008d8b8a055e0c4956a7e132e3444
jaeyounging <87270448+jaeyounging@users.noreply.github.com>
2023-09-01 15:22:38 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    if (n <= 7) {
+        return 1
+    } else {
+        if (n % 7 === 0) {
+            return n / 7
+        } else {
+            return parseInt(n / 7) + 1
+        }
+    }
+}
