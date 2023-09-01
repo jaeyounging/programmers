e6d028d4cb8d69796e22b04a48d390e31837f061
jaeyounging <87270448+jaeyounging@users.noreply.github.com>
2023-09-01 14:47:50 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,13 +1,24 @@
-function solution(n) {
+// function solution(n) {
+//     let answer = []
+    
+//     for (let i = 1; i <= n; i++) {
+//         answer.push(i)
+//     }
+    
+//     answer = answer.filter((item) => {
+//         return item % 2 !== 0
+//     })
+    
+//     return answer
+// }
+
+
+const solution = (n) => {
     let answer = []
     
-    for (let i = 1; i <= n; i++) {
+    for (let i = 1; i <= n; i += 2) {
         answer.push(i)
     }
-    
-    answer = answer.filter((item) => {
-        return item % 2 !== 0
-    })
     
     return answer
 }
