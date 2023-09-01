9305d00dfa8daa71fe950f10db2630724e4f36a9
jaeyounging <87270448+jaeyounging@users.noreply.github.com>
2023-09-01 15:24:53 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -1,11 +1,13 @@
-function solution(n) {
-    if (n <= 7) {
-        return 1
-    } else {
-        if (n % 7 === 0) {
-            return n / 7
-        } else {
-            return parseInt(n / 7) + 1
-        }
-    }
-}
+// function solution(n) {
+//     if (n <= 7) {
+//         return 1
+//     } else {
+//         if (n % 7 === 0) {
+//             return n / 7
+//         } else {
+//             return parseInt(n / 7) + 1
+//         }
+//     }
+// }
+
+const solution = (n) => Math.ceil(n / 7)
