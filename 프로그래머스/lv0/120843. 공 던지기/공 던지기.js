a5a4699660503082d0c6fb3ef85ad6d682c15ed7
jaeyounging <87270448+jaeyounging@users.noreply.github.com>
2023-09-10 20:58:59 +0900

    [level 0] Title: 공 던지기, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
@@ -0,0 +1,17 @@
+// 1. k번 만큼 배열 인덱스에 2를 더하며 공을 던짐
+// 2. 인덱스 번호가 배열의 길이 이상이면 배열의 길이만큼 빼줌
+// 3. 마지막으로 나온 배열의 인덱스의 원소를 반환
+
+function solution(numbers, k) {
+    let index = 0
+    
+    for (let i = 0; i < k - 1; i++) {
+        index += 2
+        
+        if (index >= numbers.length) {
+            index -= numbers.length
+        }              
+    }
+    
+    return numbers[index]
+}
