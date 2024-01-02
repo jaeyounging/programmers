3f1f1800a0ced601e5ce85b998391acee5b704c4
jaeyounging <87270448+jaeyounging@users.noreply.github.com>
2024-01-02 10:41:06 +0900

    [level 1] Title: 예산, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,17 +1,4 @@
 function solution(d, budget) {
-    let sum = 0;
-    let answer = 0;
-    
-    d.sort((a, b) => a - b); // 오름차순 정렬
-    
-    d.forEach((item) => {
-        sum += item;
-        answer++; 
-        
-        if (sum > budget) {
-            answer--;
-        } 
-    })
-    
+    var answer = 0;
     return answer;
 }
