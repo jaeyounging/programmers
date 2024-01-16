f94d28869ef251dc279e9f536842a14a36889c11
jaeyounging <87270448+jaeyounging@users.noreply.github.com>
2024-01-16 12:47:00 +0900

    [level Lv.1] Title: 폰켓몬, Time: 0.22 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/Lv.1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/Lv.1/1845. 폰켓몬/폰켓몬.js
@@ -1,9 +1,5 @@
 function solution(nums) {
     let set = new Set(nums);
     
-    if (nums.length / 2 < set.size) {
-        return nums.length / 2;
-    } else {
-        return set.size;
-    }
+    return nums.length / 2 < set.size ? nums.length / 2 : set.size
 }
